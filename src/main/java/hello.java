549bd9a0ab19d2ccbbc21fdac028a11f3da3e3db
yangbyeonghun <21900428@handong.ac.kr>
2022-09-02 17:22:50 +0900

    create hello project2

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,6 +2,7 @@ public class hello {
     private static void main(String[] args){
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
 
     }
 }
