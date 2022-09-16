37a58af1923dd693555767969e21f7f73cc72c0b
yangbyeonghun <21900428@handong.ac.kr>
2022-09-16 17:49:14 +0900

    create hello project2

--- src/main/java/WordManager.java
+++ src/main/java/WordManager.java
@@ -17,7 +17,11 @@ public class WordManager {
             System.out.println("*** 영단어 마스터 ***");
             System.out.println("******************");
             System.out.println("1. 모든 단어 보기");
+            System.out.println("2. 수준별 단어 보기");
+            System.out.println("3. 단어 검색");
             System.out.println("4. 단어 추가");
+            System.out.println("5. 단어 수정");
+            System.out.println("6. 단어 삭제");
             System.out.println("7. 파일 저장");
             System.out.println("0. 나가기");
             System.out.println("******************");
@@ -30,6 +34,14 @@ public class WordManager {
             else if(num==4)
             {
                 word.create();
+            }
+            else if(num==5)
+            {
+                word.updateItem();
+            }
+            else if(num==6)
+            {
+                word.deleteItem();
             }
             else if(num==7)
             {
